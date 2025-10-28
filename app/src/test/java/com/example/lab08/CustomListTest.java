15e050652922399cb5530c280dd4bb5270c60380
maansi <maansisharma26@gmail.com>
2025-10-28 17:43:14 -0600

    initial update

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,18 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList(null, new ArrayList<City>());
+
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+
+        assertTrue(list.hasCity(city));
+
+        City otherCity = new City("Calgary", "AB");
+        assertFalse(list.hasCity(otherCity));
+    }
 
 }
+
